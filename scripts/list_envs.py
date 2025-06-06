b0de6c266a38aee4cd677d1cc2c28cf7957a4c55
Danil Krivonogov <555kd@mail.ru>
2025-06-06 15:13:53 +0300

    refactor: pre-commit all files

--- scripts/list_envs.py
+++ scripts/list_envs.py
@@ -25,9 +25,8 @@ simulation_app = app_launcher.app
 """Rest everything follows."""
 
 import gymnasium as gym
-from prettytable import PrettyTable
-
 import RL_Local_Planner.tasks  # noqa: F401
+from prettytable import PrettyTable
 
 
 def main():
